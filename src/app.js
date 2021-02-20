4fb8bdad224dd6d62d79c4b80d73ee13f0db720e
Chams <atchayasuresh17@gmail.com>
2021-02-20 17:10:42 +0530

    1721994

--- src/app.js
+++ src/app.js
@@ -27,6 +27,12 @@ class Player {
         // Create member variables and assign values
         // Type your code
 
+        this.id=id;
+        this.name=name;
+        this.strength=this.getRandomStrength();
+        this.image="images/super-"+(id+1)+".png";
+        this.type=type;
+
     }
 
     // getting random strength
@@ -39,7 +45,27 @@ class Player {
         // Accumulate HTML template
         // Type your code here
 
-    
+        let player=document.createElement('div');
+        player.classList.add("player");
+        player.setAttribute('data-id',this.id);
+
+        let image=document.createElement('img');
+        image.setAttribute('src',this.image);
+
+        let name=document.createElement('div');
+        name.classList.add("name");
+        name.innerHTML=this.name;
+
+        let strength=document.createElement('div');
+        strength.classList.add("strength");
+        strength.innerHTML=this.strength;
+
+        let type=document.createElement('div');
+        
+        type.setAttribute("type",this.type);
+
+        player.append(image,name,strength,type);
+
         return player;
     }
 }
@@ -50,6 +76,13 @@ class Superwar {
     // Create a field players 
     // Use Map method to loop through players argument and create new players
     // Type your code here
+
+    let id=0;
+    this.players=players.map((player,id)=>{
+       
+        let type=(id%2==0)?"hero":"villain";
+        return new Player(id,player,type);
+    });
 
     }
 
